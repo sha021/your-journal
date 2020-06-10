945afddf1d14aa24ef388304cce9397b8bf349c4
Siena Ha <sha021@ucr.edu>
2020-06-10 05:59:01 -0700

    while loop - close with bye

--- chat.py
+++ chat.py
@@ -8,21 +8,22 @@ DIALOGFLOW_PROJECT_ID = 'journal-wkwhjy'
 DIALOGFLOW_LANGUAGE_CODE = 'en'
 SESSION_ID = 'me'
 
+text_to_be_analyzed = ''
 
 print('======================= Your Journal ========================')
 print('--------- Type bye if you want to close the Journal ---------')
-text_to_be_analyzed = input("You: ")
-session_client = dialogflow.SessionsClient()
-session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
-query_input = dialogflow.types.QueryInput(text=text_input)
-try:
-    response = session_client.detect_intent(session=session, query_input=query_input)
-except InvalidArgument:
-    raise
 
-
-print('Query text:', response.query_result.query_text)
-print('Detected intent:', response.query_result.intent.display_name)
-print('Detected intent confidence:', response.query_result.intent_detection_confidence)
-print('Fulfillment text:', response.query_result.fulfillment_text)
+while (text_to_be_analyzed != 'bye') :
+    text_to_be_analyzed = input("You: ")
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
+    print('Query text:', response.query_result.query_text)
+    print('Detected intent:', response.query_result.intent.display_name)
+    print('Detected intent confidence:', response.query_result.intent_detection_confidence)
+    print('Fulfillment text:', response.query_result.fulfillment_text)
