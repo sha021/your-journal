37d58cb304c7370ca2d0da82b4743354a7cae20c
Nathan D. Brennan <nbren004@ucr.edu>
2020-06-11 04:19:02 -0700

    Can post journal entries to Firebase

--- chat.py
+++ chat.py
@@ -1,38 +1,62 @@
 import os
 import dialogflow
 from google.api_core.exceptions import InvalidArgument
+from firebase import firebase
+from datetime import datetime
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'privateKey.json'
+def getUser():
+    name = input('Please enter your name: ')
+    if not name:
+        return 'Guest'
+    else:
+        return name
 
-DIALOGFLOW_PROJECT_ID = 'journal-wkwhjy'
-DIALOGFLOW_LANGUAGE_CODE = 'en'
-SESSION_ID = 'me'
+def runDialog():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'privateKey.json'
 
+    DIALOGFLOW_PROJECT_ID = 'journal-wkwhjy'
+    DIALOGFLOW_LANGUAGE_CODE = 'en'
+    SESSION_ID = 'me'
 
-text_to_be_analyzed = ''
-conversation = ''
-record = 0
+    # Initialize Firebase Entry
+    firebaseApp = firebase.FirebaseApplication('https://journal-wkwhjy.firebaseio.com/', None)
+    timeStamp = datetime.now().strftime(f'%d-%m-%Y+%H:%M:%S')
+    name = getUser()
+    section = name + '/' + timeStamp
 
-print('\n======================= Your Journal ========================')
-print('-------- Type "bye" if you want to close the Journal --------\n')
+    text_to_be_analyzed = ''
+    conversation = []
+    record = False
 
-while text_to_be_analyzed != 'bye':
-    text_to_be_analyzed = input('You: ')
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-    text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
-    query_input = dialogflow.types.QueryInput(text=text_input)
-    try:
-        response = session_client.detect_intent(session=session, query_input=query_input)
-    except InvalidArgument:
-        raise
-    if (response.query_result.intent.display_name == 'record'):
-        record = 1
-    if (record > 0):
-        conversation += text_to_be_analyzed + '\n'
-    # print('\nQuery text:', response.query_result.query_text)
-    print('\nDetected intent:', response.query_result.intent.display_name)
-    print('Detected intent confidence:', response.query_result.intent_detection_confidence)
-    print('\nYour Journal:', response.query_result.fulfillment_text)
+    print('\n======================= Your Journal ========================')
+    print('-------- Type "bye" if you want to close the Journal --------\n')
 
-print('\n', conversation, sep='')
+    while text_to_be_analyzed != 'bye':
+        text_to_be_analyzed = input('You: ')
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+        text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        try:
+            response = session_client.detect_intent(session=session, query_input=query_input)
+        except InvalidArgument:
+            raise
+        if (response.query_result.intent.display_name == 'record'):
+            record = True
+        if (record):
+            conversation.append(text_to_be_analyzed)
+        # print('\nQuery text:', response.query_result.query_text)
+        print('\nDetected intent:', response.query_result.intent.display_name)
+        print('Detected intent confidence:', response.query_result.intent_detection_confidence)
+        print('\nYour Journal:', response.query_result.fulfillment_text)
+
+    print('\n', conversation, sep='')
+    # conversationText = '. '.join(conversation)
+    # print(conversationText)
+    
+
+    result = firebaseApp.post(f'JournalEntries/{section}', conversation)
+    print('Entry Name:', result)
+
+if __name__ == '__main__':
+    runDialog() 
